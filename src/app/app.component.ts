ded71df24b1f51a99a5b47f2afd0baa2f548f146
karthikyadav3 <48982688+karthikyadav3@users.noreply.github.com>
2019-11-13 16:20:54 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule} from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+ServerStatus= 'offline';
+state=false;
+serverName='';
+serverID='';
+
+buttonState= true;
+statusFlag=false;
+
+constructor(){
+  setTimeout(() =>{
+    this.buttonState= false;
+  },2000);
 }
+toggleServerStatus(){
+  this.statusFlag= !this.statusFlag;
+  if(this.statusFlag==true){
+    this.ServerStatus='online';
+  }else{
+    this.ServerStatus='offline';
+    }
+  }
+reset(){
+  this.serverName='';
+  this.serverID='';
+  }
+check(){
+  if(this.serverName==''){
+    this.state=true;
+    return this.state;
+  }
+}
+}
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './servers/server.component';
+import { RedComponent } from './red/red.component';
+import { YellowComponent } from './yellow/yellow.component';
+import { GreenComponent } from './green/green.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    RedComponent,
+    YellowComponent,
+    GreenComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
